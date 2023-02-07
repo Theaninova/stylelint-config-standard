b348c21d421773db9907bc062dbfd58fcb3bc13c
Thea Schöbl <dev@theaninova.de>
2023-02-07 20:32:59 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 module.exports = {
-    "extends": [
-        "stylelint-config-standard",
-        "stylelint-config-recommended-scss",
-        "stylelint-config-html/svelte",
-        "stylelint-config-clean-order",
-        "stylelint-config-prettier"
-    ],
-    "rules": {
-        "selector-pseudo-class-no-unknown": [
-            true,
-            {
-                "ignorePseudoClasses": ["global"]
-            }
-        ]
-    }
+  "extends": [
+    "stylelint-config-standard-scss",
+    "stylelint-config-recommended-scss",
+    "stylelint-config-html/svelte",
+    "stylelint-config-clean-order",
+    "stylelint-config-prettier-scss"
+  ],
+  "rules": {
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        "ignorePseudoClasses": ["global"]
+      }
+    ]
+  }
 }
